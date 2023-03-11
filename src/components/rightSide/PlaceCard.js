3f38b049faf9bdeced71dbd116615ede44712ae0
Youssef Rashad <egyoussef_2000@hotmail.com>
2023-03-11 23:18:22 +0200

    api error

--- src/components/rightSide/PlaceCard.js
+++ src/components/rightSide/PlaceCard.js
@@ -1,65 +1,61 @@
-import { AiFillDislike, AiFillLike, AiFillStar } from 'react-icons/ai'
-import { useEffect, useState } from 'react';
-import './PlaceCard.css'
-const PlaceCard=(props)=>{
-    const [beachPlaces, setBeachPlaces] = useState([]);
- async function fetchPlacesHandlers() {
-        const response =
-          await fetch(`https://api.tomtom.com/search/2/categorySearch/Beach.json?limit=100&lat=31.261193878823768&lon=32.306969060980315&radius=10000&view=Unified&relatedPois=all&key=6xSTnZiuQ9q3oaOLOIyVbzH8fjqKOA1H`);
+import { AiFillStar } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import "./PlaceCard.css";
+const PlaceCard = (props) => {
+  const [beachPlaces, setBeachPlaces] = useState([]);
+  async function fetchBeachPlaces() {
+    const response = await fetch(
+      `https://api.tomtom.com/search/2/categorySearch/Beach.json?limit=100&lat=31.261193878823768&lon=32.306969060980315&radius=10000&view=Unified&relatedPois=all&key=6xSTnZiuQ9q3oaOLOIyVbzH8fjqKOA1H`);
+
+    const data = await response.json();
+
+    const transformedData = data.results.map((takeAwayData) => {
+      return {
+        id: takeAwayData.id,
+        header: takeAwayData.poi.name,
+        street: takeAwayData.address.streetName,
+        city: takeAwayData.address.localName,
+        type: takeAwayData.poi.categories[0],
+        distance: takeAwayData.dist,
+      };
+    });
     
-        const data = await response.json();
-    
-        const transformedData = data.results.map((takeAwayData) => {
-          return {
-            id: takeAwayData.id,
-            header: takeAwayData.poi.name,
-            street: takeAwayData.address.streetName,
-            city: takeAwayData.address.localName,
-            type: takeAwayData.poi.categories[0],
-            distance: takeAwayData.dist,
-          };
-        }
-        );
-        setBeachPlaces(transformedData);
-        console.log(beachPlaces)
+    setBeachPlaces(transformedData);
+    console.log(transformedData)
+  }
 
+  useEffect(() => {
+    fetchBeachPlaces();
 
-}
+  }, []);
 
-useEffect(() => {
-    fetchPlacesHandlers()
-
-  },[])
-  
-
-     
-      
-    return(<>
-        <div id='card__container'>
-            <div id='card__firstRow'>
-            <img src='/assets/images/Nearby__images/1.jpg' alt='img'></img>
+  return (
+    <>
+      {beachPlaces.map((takeAwayData) => (
+        <div id="card__container">
+          <div id="card__firstRow">
+            <img src="/assets/images/Nearby__images/1.jpg" alt="img"></img>
+          </div>
+          <div className="imageInfo">
+            <p id="cardHeader">{takeAwayData.header}</p>
+            <div className="star__container">
+              <AiFillStar></AiFillStar>
+              <AiFillStar></AiFillStar>
+              <AiFillStar></AiFillStar>
+              <AiFillStar></AiFillStar>
+              <AiFillStar></AiFillStar>
             </div>
-            <div className='imageInfo'>
-            <p  id='cardHeader'>Place Name</p>
-            <button >bla</button>
-            <div className='star__container'>
-            <AiFillStar></AiFillStar>
-            <AiFillStar></AiFillStar>
-            <AiFillStar></AiFillStar>
-            <AiFillStar></AiFillStar>
-            <AiFillStar></AiFillStar>
+            <div id="card__par">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+                vitae condimentum lectus, non pharetra ligula. Etiam sit amet
+                odio at tortor consectetur bibendum
+              </p>
             </div>
-            <div id='card__par'>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae condimentum lectus, non pharetra ligula. Etiam sit amet odio at tortor consectetur bibendum</p>
-            </div>
-            <div className='card__like'>
-            <AiFillLike className='faLike'></AiFillLike>
-            <AiFillDislike className='faLike'></AiFillDislike>
-            </div>
-            </div>
+          </div>
         </div>
-   
- 
-    </>)
-}
-export default PlaceCard
+      ))}
+    </>
+  );
+};
+export default PlaceCard;
